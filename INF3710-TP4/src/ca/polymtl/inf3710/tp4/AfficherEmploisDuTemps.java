6effc65c8b8b27a0fcf85a580fa3cd865caa1dbe
Benjamin O'Connell-Armand <benjamin.oconnell@polymtl.ca>
2013-11-12 13:47:44 -0500

    Correction du nom de package.

--- INF3710-TP4/src/ca/polymtl/inf3710/tp4/AfficherEmploisDuTemps.java
+++ INF3710-TP4/src/ca/polymtl/inf3710/tp4/AfficherEmploisDuTemps.java
@@ -1,4 +1,4 @@
-package ca.polymtl.inf3710;
+package ca.polymtl.inf3710.tp4;
 
 public class AfficherEmploisDuTemps
 {
@@ -6,7 +6,7 @@ public class AfficherEmploisDuTemps
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		
+		sql::
 	}
 	
 }
